b4bc5d4fbab1bb0fa12b11ff240d14ad21a6ed66
Mike97M <mike97m@yahoo.com>
2017-04-27 19:09:42 +0300

    Add files via upload

---
+++ Exceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace API2Captcha
+{
+    public class BalanceException : Exception
+    {
+        public BalanceException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+    public class ResponseException : Exception
+    {
+        public ResponseException(string message) : base(message)
+        {
+
+        }
+    }
+}
